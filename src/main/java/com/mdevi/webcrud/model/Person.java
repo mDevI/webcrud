4970d7fb2db5752943f273286bc20b11cb4f020a
Sergei.Belonosov <2belonosov@gmail.com>
2018-08-29 00:29:57 +0700

    FIX: a bug to save person. Type Person.id was changed to Long.

--- src/main/java/com/mdevi/webcrud/model/Person.java
+++ src/main/java/com/mdevi/webcrud/model/Person.java
@@ -11,7 +11,7 @@ public class Person {
 
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
@@ -25,11 +25,11 @@ public class Person {
         this.email = email;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
